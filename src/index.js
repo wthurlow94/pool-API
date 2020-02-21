14fbbaf25631451fa26d75b0956f6ed96daf85cc
Will Thurlow <will.thurlow@sgdigital.com>
2020-02-21 13:19:37 +0000

    Skeleton API

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors'
 
+import routes from './routes';
+
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/users', routes.user);
 
 app.listen(process.env.PORT, () =>
 	console.log(`Example app listening on port ${process.env.PORT}!`),
