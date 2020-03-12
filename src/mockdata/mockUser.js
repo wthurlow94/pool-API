c98f273523b7028b83b2ad6043f3bbefbfc5f07b
Will Thurlow <will.thurlow@sgdigital.com>
2020-03-12 12:49:10 +0000

    create user using email and password. \n confirm email does not already exist before creating

--- src/mockdata/mockUser.js
+++ src/mockdata/mockUser.js
@@ -5,24 +5,26 @@ import crypto from 'crypto';
 
 casual.define('user', function () {
 	return {
-		id:casual.uuid,
-		username:casual.username,
-		email:casual.email,
-		password:""
+		id:casual.integer(1,1000),
+		email:"",
+		password:"",
+		elo:400
+
 	};
 });
 
 
 const data = { users: [] };
 
-function addUser (password) {
-	
+function addUser (email, password) {
+	//todo: move password functionality to an external util
 	let salt = crypto.randomBytes(16).toString('base64');
 	let hash = crypto.createHmac('sha512',salt).update(password).digest("base64");
 
 	password = salt+"$"+hash;
 	var user = casual.user;
 	user.password = password;
+	user.email = email;
 	data.users.push(user);
 	return user;
 }
--- src/routes/mock/mockUser.js
+++ src/routes/mock/mockUser.js
@@ -22,7 +22,13 @@ router.get('/', (req, res) => {
 //register new user
 router.post('/', (req, res) => {
 	
-	res.send(user.addUser(req.body.password));
+	if (user.findUserByMail(req.body.email).length >= 1) {
+		// user doesn't exist
+		res.json({"success":false,"message":"Email already exists"}).send();
+		return
+		
+	}
+	res.send(user.addUser(req.body.email,req.body.password));
 
 });
 
